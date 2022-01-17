d8072f9efe14ea71667a7bd46dcb78787c825259
TimW <"wyattim@gmail.com">
2022-01-17 20:04:58 +0000

    Changes to menu class

--- src/com/lockedme/companylockers/Main.java
+++ src/com/lockedme/companylockers/Main.java
@@ -1,6 +1,6 @@
 package com.lockedme.companylockers;
 
-import java.util.Scanner;
+
 
 public class Main {
 	
@@ -18,12 +18,19 @@ public class Main {
 	public static void main(String[] args) {
 		
 		int menuItem = 0;
+		
 		String[] menuItems = {"first","second","third"};
+		menu mainMenu = new menu(menuItems);
 		banner b = new banner("app", "Tim Wyatt","wyattim@gmail.com");
 		b.printBanner();
+		
 		//Need a loop to go around the menu, until a quit is detected
-		menu m = new menu(menuItems);
-		m.printMenu();
+		
+		mainMenu.printMenu();
+		menuItem = mainMenu.getUserInput();
+		String menuString = new String();
+		menuString = mainMenu.getMenuItem(menuItem-1); //bit of a hack to get the array element
+		System.out.println("Entered " + menuItem  + ". " + menuString);
 		
 		
 	}
--- src/com/lockedme/companylockers/menu.java
+++ src/com/lockedme/companylockers/menu.java
@@ -1,20 +1,43 @@
 package com.lockedme.companylockers;
 
+import java.util.Scanner;
+
 public class menu {
 	
 	private String[] menuItems;
+	private String root = new String();
 	
 	public menu (String[] items) {
-		menuItems = items;
+		this.menuItems = items;
+	}
+	
+	public menu(String rootItem, String[] items) {
+		this.root = rootItem;
+		this.menuItems = items;
 	}
 
 	public String[] getMenuArray() {
 		return menuItems;
 	}
 	
+	public String getMenuItem(int itemNumber) {
+		return menuItems[itemNumber];
+	}
+	
+	public int getUserInput() {
+		
+		int selection;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a menu item number <enter>: ");
+		selection = sc.nextInt();
+		sc.close();
+		return selection;		
+	}
+	
 	public void printMenu() {
 		
 		for(int x=0; x < menuItems.length;x++)
-			System.out.println(x + ". " + menuItems[x]);
+			System.out.println(x+1 + ". " + menuItems[x]);
 	}
 }
