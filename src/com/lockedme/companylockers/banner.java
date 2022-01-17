1bc40b418a7b4da3b780e0b003111da994cca741
TimW <"wyattim@gmail.com">
2022-01-17 16:35:31 +0000

    Created banner class

--- src/com/lockedme/companylockers/banner.java
+++ src/com/lockedme/companylockers/banner.java
@@ -1,34 +1,33 @@
 package com.lockedme.companylockers;
 
-import java.util.Scanner;
-
-public class Main {
+public class banner {
 	
-	public int mainMenu() {
-		
-		return 0;
+	static String appCreator = new String("");
+	static String appName = new String("");
+	static String creatorEmail = new String("");
+	
+	/**
+	 * 
+	 * @param app
+	 * @param email
+	 * @param creator
+	 */
+	public banner (String app,  String email, String creator){
+		appCreator = creator;
+		creatorEmail = email;
+		appName = app;
 	}
 	
-	public int subMenu() {
-		
-		return 0;
-		
-	}
-
-	public static void printBanner() {
+	public void printBanner() {
 		System.out.println("____    __    ____  _______  __        ______   ______   .___  ___.  _______"); 
 		System.out.println("\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|");
 		System.out.println(" \\   \\/    \\/   /  |  |__   |  |     |  ,----'|  |  |  | |  \\  /  | |  |__");   
 		System.out.println("  \\            /   |   __|  |  |     |  |     |  |  |  | |  |\\/|  | |   __|  ");
 		System.out.println("   \\    /\\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____"); 
 		System.out.println("    \\__/  \\__/     |_______||_______| \\______| \\______/  |__|  |__| |_______|");
-		                                                                             
-		System.out.println("Application Name: LockedMe.com");
-		System.out.println("Written by: Tim Wyatt (tim.wyatt@vodafone.com");
-	}
-	public static void main(String[] args) {
+		System.out.println("\nApplication Name: " + appName);
+		System.out.println("Application Author: " + appCreator);
+		System.out.println("Application Author mail: " + creatorEmail);
 		
-		banner b = new banner("app", "wyattim@gmail.com", "Tim Wyatt");
-		b.printBanner();
 	}
 }
