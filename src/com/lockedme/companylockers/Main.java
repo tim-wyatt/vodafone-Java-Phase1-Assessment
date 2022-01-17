cd0d1a0f887a761988d364234561ca0d36a346fc
TimW <"wyattim@gmail.com">
2022-01-17 14:27:54 +0000

    Created welcome banner

--- src/com/lockedme/companylockers/Main.java
+++ src/com/lockedme/companylockers/Main.java
@@ -2,8 +2,23 @@ package com.lockedme.companylockers;
 
 public class Main {
 
+	public static void printBanner() {
+		System.out.println("____    __    ____  _______  __        ______   ______   .___  ___.  _______"); 
+		System.out.println("\\   \\  /  \\  /   / |   ____||  |      /      | /  __  \\  |   \\/   | |   ____|");
+		System.out.println(" \\   \\/    \\/   /  |  |__   |  |     |  ,----'|  |  |  | |  \\  /  | |  |__");   
+		System.out.println("  \\            /   |   __|  |  |     |  |     |  |  |  | |  |\\/|  | |   __|  ");
+		System.out.println("   \\    /\\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____"); 
+		System.out.println("    \\__/  \\__/     |_______||_______| \\______| \\______/  |__|  |__| |_______|");
+		                                                                             
+		System.out.println("Application Name: LockedMe.com");
+		System.out.println("Written by: Tim Wyatt (tim.wyatt@vodafone.com");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		printBanner();
+		
+
+
 
 	}
 
