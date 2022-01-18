0dee04ca3917daa322ee6244e5f746ef02929ab4
TimW <"wyattim@gmail.com">
2022-01-18 14:30:18 +0000

    Made main menu work.

--- src/com/lockedme/companylockers/Main.java
+++ src/com/lockedme/companylockers/Main.java
@@ -3,22 +3,20 @@ package com.lockedme.companylockers;
 public class Main {
 	
 	public static void main(String[] args) {
+		String menuItem = new String();
+		String[] mainMenuItems = {"Display current files","File operations"};
 		
-		int menuItem = 0;
-		
-		String[] menuItems = {"first","second","third"};
-		menu mainMenu = new menu(menuItems);
+		menu mainMenu = new menu(mainMenuItems);
 		banner b = new banner("app", "Tim Wyatt","wyattim@gmail.com");
 		b.printBanner();
 		
-		//Need a loop to go around the menu, until a quit is detected
-		
-		mainMenu.printMenu();
-		menuItem = mainMenu.getUserInput();
-		String menuString = new String();
-		menuString = mainMenu.getMenuItem(menuItem-1); //bit of a hack to get the array element
-		System.out.println("Entered " + menuItem  + ". " + menuString);
-		
-		
+		while(!menuItem.equals("q")) {
+			mainMenu.printMenu();
+			menuItem = mainMenu.getUserInput();
+			String menuString = new String();
+			menuString = mainMenu.getMenuItem(menuItem); 
+			System.out.println("Entered " + menuItem  + ". " + menuString);	
+		}
+		System.out.println("Quitting");
 	}
 }
--- src/com/lockedme/companylockers/menu.java
+++ src/com/lockedme/companylockers/menu.java
@@ -3,14 +3,14 @@ package com.lockedme.companylockers;
 import java.util.Scanner;
 
 public class menu {
-	
+
 	private String[] menuItems;
 	private String root = new String();
-	
-	public menu (String[] items) {
+
+	public menu(String[] items) {
 		this.menuItems = items;
 	}
-	
+
 	public menu(String rootItem, String[] items) {
 		this.root = rootItem;
 		this.menuItems = items;
@@ -19,25 +19,53 @@ public class menu {
 	public String[] getMenuArray() {
 		return menuItems;
 	}
-	
-	public String getMenuItem(int itemNumber) {
-		return menuItems[itemNumber];
-	}
-	
-	public int getUserInput() {
-		
-		int selection;
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a menu item number <enter>: ");
-		selection = sc.nextInt();
-		sc.close();
-		return selection;		
+	public String getMenuArrayItem(int index) {
+		return menuItems[index];
 	}
-	
+
+	public String getMenuItem(String itemLetter) {
+		int baseAscii = 97;
+		int itemIndex = 0;
+		char itemLetterChar;
+
+		String returnItem = new String();
+		if (!itemLetter.equals("q")) {
+			itemLetterChar = itemLetter.charAt(0);
+			itemIndex = (int) itemLetterChar - baseAscii;
+			if (itemIndex > menuItems.length - 1 || itemIndex < 0) {
+				returnItem = "Error: You have selected an option that isn't available";
+			} else
+				returnItem = menuItems[itemIndex];
+		} else
+			returnItem = "q";
+
+		return returnItem;
+	}
+
+	public String getUserInput() {
+
+		String selection = new String();
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			System.out.print("Enter a menu item number <q to quit>: ");
+			selection = sc.next();
+		} catch (Exception e) {
+			System.out.println("An error occurred!");			
+			e.printStackTrace();
+		}
+		return selection;
+	}
+
 	public void printMenu() {
-		
-		for(int x=0; x < menuItems.length;x++)
-			System.out.println(x+1 + ". " + menuItems[x]);
+		int baseAscii = 97;
+		int asciiValue = 0;
+
+		for (int x = 0; x < menuItems.length; x++) {
+			asciiValue = baseAscii + x;
+			char menuOption = (char) asciiValue;
+			System.out.println(menuOption + ". " + menuItems[x]);
+		}
 	}
 }
